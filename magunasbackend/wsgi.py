c4b101ad4c93dd41d643b0d3ed7dd31312b8fb4e
jeankariuki5-sudo <jeankariuki5@gmail.com>
2026-07-22 22:00:04 +0300

    finalise

--- magunasbackend/wsgi.py
+++ magunasbackend/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+sys.path.append("/home/jeanwanjiru/www/magunasbackend")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magunasbackend.settings')
 
